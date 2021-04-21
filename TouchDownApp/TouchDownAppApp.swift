c8fb28d0de46a66abde18536e36cc6814215ab73
Mike Camp <mike_camp23@icloud.com>
2021-04-21 15:50:10 -0400

    Initial Commit

---
+++ TouchDownApp/TouchDownAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchDownAppApp.swift
+//  TouchDownApp
+//
+//  Created by Michael Camp on 4/21/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchDownAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
